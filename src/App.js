6c43b948535dc800fdb4fb8b14db40f031c7e7df
Andrii-Velhan <velganaa@gmail.com>
2021-01-17 21:42:07 +0200

    updt

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default function App() {
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
+        stats={user.stats}
       />
       <Statistics title="Upload stats" stats={statisticalData} />
       <Statistics stats={statisticalData} />
